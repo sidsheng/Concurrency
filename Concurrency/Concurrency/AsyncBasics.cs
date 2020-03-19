5337ceec800238e1735162e169b8fb19c0076c4f
sidsheng <sidsheng@gmail.com>
2020-03-19 22:14:07 +1100

    2.1 and 2.2

---
+++ Concurrency/Concurrency/AsyncBasics.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Concurrency
+{
+    #region 2.2
+
+    interface IMyAsyncInterface
+    {
+        Task<int> GetValueAsync();
+
+        Task DoSomethingAsync();
+    }
+
+    class MySynchronousImplementation : IMyAsyncInterface
+    {
+        public Task<int> GetValueAsync()
+        {
+            return Task.FromResult(182);
+        }
+
+        public Task DoSomethingAsync()
+        {
+            try
+            {
+                // do something
+                _ = 1 + 2;
+
+                return Task.CompletedTask;
+            }
+            catch (Exception ex)
+            {
+                return Task.FromException(ex);
+            }
+        }
+
+        internal Task<T> NotImplementedAsync<T>()
+        {
+            return Task.FromException<T>(new NotImplementedException());
+        }
+
+        internal Task<int> GetValueAsync(CancellationToken cancellationToken)
+        {
+            if (cancellationToken.IsCancellationRequested)
+            {
+                return Task.FromCanceled<int>(cancellationToken);
+            }
+
+            return Task.FromResult(182);
+        }
+    }
+
+    #endregion
+
+    public class AsyncBasics
+    {
+        public AsyncBasics()
+        {
+        }
+
+        public async Task Start()
+        {
+            #region 2.1
+
+            await DelayResult<int>(182, TimeSpan.FromSeconds(1));
+            string result1 = await DoStuffWithRetries();
+            string result2 = await DoStuffWithTimeout();
+
+            #endregion
+
+            #region 2.2
+
+            MySynchronousImplementation mySyncImp = new MySynchronousImplementation();
+
+            int result3 = await mySyncImp.GetValueAsync();
+            Console.WriteLine($"result = {result3}");
+
+            await mySyncImp.DoSomethingAsync();
+            Console.WriteLine("DoSomethingAsync()");
+
+            try
+            {
+                await mySyncImp.NotImplementedAsync<int>();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"NotImplementedAsync {ex}");
+            }
+
+            try
+            {
+                var ct = new CancellationTokenSource();
+                ct.Cancel();
+                int result4 = await mySyncImp.GetValueAsync(ct.Token);
+                Console.WriteLine($"GetValueAsync{result4}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Cancellation {ex}");
+            }
+
+            #endregion
+        }
+
+        #region 2.1
+
+        /// <summary>
+        /// Simple Task.Delay()
+        /// </summary>
+        async Task<T> DelayResult<T>(T result, TimeSpan delay)
+        {
+            Console.WriteLine($"Delay for {delay}");
+            await Task.Delay(delay);
+
+            return result;
+        }
+
+        /// <summary>
+        /// Exponential backoff
+        /// </summary>
+        async Task<string> DoStuffWithRetries()
+        {
+            TimeSpan delay = TimeSpan.FromSeconds(1);
+            for (int i = 0; i < 3; i++)
+            {
+                try
+                {
+                    return TaskWhichThrowsException();
+                }
+                catch { }
+
+                Console.WriteLine($"Delay for {delay}");
+                await Task.Delay(delay);
+                delay = delay * 2;
+            }
+
+            return "blink";
+        }
+
+        string TaskWhichThrowsException()
+        {
+            Console.WriteLine($"Throwing Exception {DateTime.Now}");
+            throw new Exception("AsyncBasics Exception");
+        }
+
+        async Task<string> DoStuffWithTimeout()
+        {
+            using var cts = new CancellationTokenSource(TimeSpan.FromSeconds(3));
+            Task<string> work = DoTask();
+            Task timeoutTask = Task.Delay(Timeout.InfiniteTimeSpan, cts.Token);
+
+            Task completedTask = await Task.WhenAny(work, timeoutTask);
+            if (completedTask == timeoutTask)
+            {
+                Console.WriteLine("Cancelled");
+                return null;
+            }
+
+            Console.WriteLine("Work done");
+            return await work;
+        }
+
+        async Task<string> DoTask()
+        {
+            await Task.Delay(5000);
+            return "blink";
+        }
+
+        #endregion
+
+        #region 2.2
+
+
+
+        #endregion
+    }
+}
---
+++ Concurrency/Concurrency/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Concurrency
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AsyncBasics asyncBasics = new AsyncBasics();
+            asyncBasics.Start().Wait();
+        }
+    }
+}
